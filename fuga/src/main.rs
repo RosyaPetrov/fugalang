c91008fe1c903569f3222ccd7a7bc02acf6f3ad8
rp1s <140802204+rp1s@users.noreply.github.com>
2026-08-13 18:37:50 +0300

    add lexer

--- fuga/src/main.rs
+++ fuga/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    let x = '2';
+    let mut lexer = lexer::Lexer::new("let x := 42");
+
+    loop {
+        let token = lexer.next_token();
+        println!("{token:?}");
+
+        if token.token_type == token::TokenType::Eof {
+            break;
+        }
+    }
 }
